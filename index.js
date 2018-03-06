8064a0f05d59f8c3ba0db804a13a30fec671ebc3
Rajesh Sahani <rajesh.sahani@anz.com>
2018-03-06 16:02:20 +0530

    added /user as a testing api

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5577
+const express     = require("express")
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser  = require('body-parser');
+const dburl = "mongodb://rajesh:password@ds257808.mlab.com:57808/heroku_q3zf2pf8"
+const app = express();
+var db;var mclient;
+const port = 8000;
+app.use(bodyParser.urlencoded({extended: true}))
 
-express()
-  .listen(PORT, () => {console.log(`Listening on ${ PORT }`);
-    console.log();}
-  );
-  express().get('/user', function(request, response){
-    response.send('user ' + request.params.id);
-  });
+app.post('/user',(req,res) => {
+
+      var db1 = mclient.db('resturant');
+      var collection=db1.collection('users');
+
+  const book = { author: req.body.email, title:req.body.name}
+collection.insert(book,(err,results) => {
+if(err) {
+  res.send({'error': 'An error has occured'})
+}
+else{
+  //res.send(result.ops[0])
+  console.log("We are live poreteteetet"  + mclient);
+
+}
+
+
+})
+console.log(req.body);
+res.send('Hello')
+})
+
+MongoClient.connect(dburl,(err,client)  => {
+  if(err) return console.log(err)
+    mclient = client
+  app.listen(port,() =>{
+    console.log("The database Url is : "  + dburl);
+    console.log("We are live port : "  + port);
+
+  })
+})
