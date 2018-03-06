143f791a8e0b6f974ab7022ac93dddc69677de6f
Rajesh Sahani <rajesh.sahani@anz.com>
2018-03-06 12:26:28 +0530

    index.js modifiex for r&d

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5577
 
 express()
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`
-    console.log()
-  ));
-  express().get('/', function(request, response){
+  .listen(PORT, () => {console.log(`Listening on ${ PORT }`);
+    console.log();}
+  );
+  express().get('/user', function(request, response){
     response.send('user ' + request.params.id);
   });
