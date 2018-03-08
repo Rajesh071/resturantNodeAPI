60b3197fa8831dbb1b42c538d432d02f1d5ece46
Rajesh Sahani <rajesh.sahani@anz.com>
2018-03-08 11:36:20 +0530

    Changed the port from hardcoded to proccess.env.port

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const dburl = "mongodb://rajesh:password@ds257808.mlab.com:57808/heroku_q3zf2pf8
 
 const app = express();
 var db;var mclient;
-const port = 8010;
+const PORT = process.env.PORT || 5000
+;
 app.use(bodyParser.urlencoded({extended: true}))
 
 function handleError(res, reason, message, code) {
@@ -41,7 +42,7 @@ console.log(req.body);
 })
 
 app.get('/',(req,res) => {
-  res.send("use https://resturant-node-api.herokuapp.com/userProfile to create user");
+  res.send("Use 'https://resturant-node-api.herokuapp.com/userProfile' to create new user");
 })
 
 
@@ -49,9 +50,9 @@ app.get('/',(req,res) => {
 MongoClient.connect(dburl,(err,client)  =>        {
                             if(err) return console.log(err)
                             mclient = client
-                            app.listen(port,() =>{
+                            app.listen(PORT,() =>{
                             console.log("The database Url is : "  + dburl);
-                            console.log("We are live port : "  + port);
+                            console.log("We are live port : "  + PORT);
 
                             })
                             })
