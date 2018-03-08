c93b7e17816bcd829e7e0a101bddf2d7eada776b
Rajesh Sahani <rajesh.sahani@anz.com>
2018-03-08 11:59:02 +0530

    Handeled the userProfile requst error

--- index.js
+++ index.js
@@ -16,13 +16,16 @@ function handleError(res, reason, message, code) {
 }
 
 app.post('/userProfile',(req,res) => {
-
+  if (!(req.body.email && req.body.name && req.body.msisdn)) {
+     handleError(res, "Invalid user input", "Must provide all three field data", 400);
+     return
+   }
       var db1 = mclient.db('heroku_q3zf2pf8');
       var collection=db1.collection('UserProfile');
       console.log("We are live 12" + collection);
       console.log("Response object is "  + res);
 
-  const book = { author: req.body.email, title:req.body.name}
+  const book = { email: req.body.email, name:req.body.name,mobileno:req.body.msisdn,}
 collection.insert(book,(err,results) => {
 if(err) {
   res.send({'error': 'An error has occured'})
